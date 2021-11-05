c31c4c285dd606df01b82bcee8eb7c1b522db0fb
Tiago Meireles <tiagocf475@gmail.com>
2021-11-05 19:02:13 -0300

    n sei o q to fazendo

--- script.js
+++ script.js
@@ -32,3 +32,32 @@ tutu.addEventListener('click', (e) => {
     e.target.classList.remove('not-selecionado')
   }
 });
+
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', (e) => {
+  const selecionado = document.querySelector('.selecionado');
+  for (let i=0; i < tutu.children.length;i++) {
+    tutu.children[i].classList.add('not-comfirmado');
+  }
+  let position = Math.floor(selecionado.getBoundingClientRect().x);
+  let root = document.documentElement;
+  console.log(position);
+  tutu.style.display = 'block'
+  tutu.style.transform = 'translateX(0)'
+  // tutu.style.width = '1000px'
+  selecionado.classList.remove('not-comfirmado')
+  selecionado.classList.add('comfirmado');
+  // if ()
+  root.style.setProperty('--position', `${position - 250}px`);
+  // console.log(root.style.setProperty('--position', `${position}px`));
+});
+
+ // let position = Math.floor(selecionado.getBoundingClientRect().left);
+  // // selecionado.style.left = `${36}px`
+  // console.log(position);
+  // while (position > 36) {
+  //   console.log(position);
+  //   position-= 1;
+  //   selecionado.style.left = `${position}px`
+  // }
